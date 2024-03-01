54ddbf4f733763dc99d1a55803a2fe663a1fee47
Pernilla Sterner <psterner@hotmail.com>
2024-03-01 15:09:59 +0100

    Add title, profile-image and about me text

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,5 +1,23 @@
 import "./Intro.scss";
 
 export const Intro = () => {
-  return <p>Intro</p>;
+  return (
+    <section className="intro-container">
+      <div className="intro-wrapper">
+        <p className="intro-text">Hi, I’m Pernilla Sterner</p>
+        <h1 className="job-title">Frontend Developer</h1>
+
+        <div className="about-me-container">
+          <div className="profile-image"></div>
+          <div className="about-me-text">
+            Pernilla is an exceptional developer known for her innovative
+            solutions and exceptional coding abilities. She creates
+            user-friendly applications and solves complex issues with ease. Her
+            drive for excellence makes her a valuable asset to any project and a
+            standout in the technology field.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
