9d7a1c3e62a0107bf420ce597dd4cfa12dfaee29
Pernilla Sterner <psterner@hotmail.com>
2024-03-10 17:45:53 +0100

    Make heading components to use for intro section

---
+++ src/components/styles/typography/Paragraph.jsx
@@ -0,0 +1,5 @@
+import "./typography.scss";
+
+export const Paragraph = ({ text, className }) => {
+  return <p className={className}>{text}</p>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Intro } from "../components/sections/Intro/Intro";
+import { Header } from "../components/sections/Header/Header";
 import { Projects } from "../components/sections/Projects/Projects";
 import { Tech } from "../components/sections/Tech/Tech";
 import "./Home.scss";
@@ -6,7 +6,7 @@ import "./Home.scss";
 export const Home = () => {
   return (
     <main>
-      <Intro />
+      <Header />
       <Tech />
       <Projects />
     </main>
