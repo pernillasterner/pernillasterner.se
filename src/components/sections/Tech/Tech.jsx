7517bb8a03763e2f0482f3431b6aac8ce4a8657f
Pernilla Sterner <psterner@hotmail.com>
2024-03-10 18:00:56 +0100

    Import heading and paragraph components

--- src/components/sections/Tech/Tech.jsx
+++ src/components/sections/Tech/Tech.jsx
@@ -1,17 +1,17 @@
+import { Heading } from "../../styles/typography/Heading";
+import { Paragraph } from "../../styles/typography/Paragraph";
 import "./Tech.scss";
 
 export const Tech = () => {
+  const techStack =
+    "HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub.";
   return (
     <section className="tech-section">
       <div className="tech-container">
-        <h2>Tech</h2>
+        <Heading level={2} text={"Tech"} />
 
         <div className="tech-skills">
-          <p>
-            HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks,
-            Node.js, Mongo DB, Web Accessibility, APIs, mob-programming,
-            pair-programming, GitHub.
-          </p>
+          <Paragraph text={techStack} className={"tech-p"} />
         </div>
       </div>
     </section>
