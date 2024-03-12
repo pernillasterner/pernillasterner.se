e30b8ff1e35e5684cbb319001729c5324aede14c
Pernilla Sterner <psterner@hotmail.com>
2024-03-12 12:44:46 +0100

    Adjust styling to header and comment out my words until new article (#10)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
       <Header />
       <Tech />
       <Projects />
-      <MyWords />
+      {/* <MyWords /> */}
       <Skills />
       <Footer />
       <FooterBanner />
